04249d83eba47befbc35c13f5666c3e1f890deb5
Zihang Gao <hello@gaozihang.com>
2023-08-29 04:24:00 +0800

    Deployed 775c916 with MkDocs version: 1.4.2

---
+++ assets/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4abde16c30820553709e143b79d036ee";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
